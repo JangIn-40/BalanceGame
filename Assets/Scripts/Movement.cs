749c28f86a1d5172c1a72ed532da211d9e1b8267
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-05-13 23:01:54 +0900

    캐릭터 움직임

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Whathappen : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //이건 내가 무엇을 해야할지 몰라서 그냥 깃허브잔디채우고 싶어서 넣는 칸이다 앞으론 이러진 말자 이러지 말자
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            transform.position = Vector2.left;
+        }
     }
 }
